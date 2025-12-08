3f7a8ad5fd581232631e824b522cfc7018228824
YanKarpov <KarpovYAA23@spb.ithub.ru>
2025-12-08 16:15:05 +0300

    Вынесение папки с тестовыми сценариями на корневой уровень для упрощения

--- StormRouterConsole/Program.cs
+++ StormRouterConsole/Program.cs
@@ -9,7 +9,7 @@ class Program
 {
     static void Main()
     {
-        string jsonPath = "../StormRouterVisualization/Data/route_data.json"; 
+        string jsonPath = "../Data/route_data.json"; 
 
         if (!File.Exists(jsonPath))
         {
